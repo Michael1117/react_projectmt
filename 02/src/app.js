1ae1fc1bdb8630e769f26f24eee4b64a2b72c1a1
Michael1117 <1322720377@qq.com>
2019-04-02 16:08:57 +0800

    react-init

--- 02/src/app.js
+++ 02/src/app.js
@@ -1,3 +1,10 @@
-require('./index.scss')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from './component/Footer/Footer';
+import Footer from './component/Header/Header'
 
-console.log('webpack')
+ReactDOM.render(<div>
+    <h1>hello world</h1>
+    <Header/>
+    <Footer/>
+</div>,document.getElementById('root'))
